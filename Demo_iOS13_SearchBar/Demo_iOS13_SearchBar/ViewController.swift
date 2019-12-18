a436aa42fc92a2b14bcdd9e73a0a9cfbb8c01b99
Bhargav Thakkar <bhargavthakkar@promactinfo.com>
2019-12-18 17:49:51 +0530

    setup

---
+++ Demo_iOS13_SearchBar/Demo_iOS13_SearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo_iOS13_SearchBar
+//
+//  Created by Bhargav Thakkar on 18/12/19.
+//  Copyright © 2019 Bhargav Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
