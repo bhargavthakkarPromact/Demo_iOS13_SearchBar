a38f92e6da4e4c483513dd8a279c502c19d7458b
Bhargav Thakkar <bhargavthakkar@promactinfo.com>
2019-12-18 18:08:36 +0530

    changes for demo

--- Demo_iOS13_SearchBar/Demo_iOS13_SearchBar/ViewController.swift
+++ Demo_iOS13_SearchBar/Demo_iOS13_SearchBar/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let search = UISearchController(searchResultsController: nil)
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Type something here..."
+        search.searchBar.scopeButtonTitles = ["Title", "Genre", "Rating", "Actor"]
+        navigationItem.searchController = search
+        //Adding token to the search bar using Search Token Class
+        let iosToken = UISearchToken(icon: UIImage(systemName: "tag"), text: "iOS")
+        let shopToken = UISearchToken(icon: UIImage(systemName: "cart.fill"), text: "Shop")
+        search.searchBar.searchTextField.insertToken(iosToken, at: 0)
+        search.searchBar.searchTextField.insertToken(shopToken, at: 1)
+        search.searchBar.searchTextField.tokenBackgroundColor = .systemBlue
+        //disable the copying and deletion of search
+        search.searchBar.searchTextField.allowsCopyingTokens = false
+        search.searchBar.searchTextField.allowsDeletingTokens = false
+        search.searchBar.searchTextField.clearButtonMode = .never
     }
-
-
+    
+    
 }
 
